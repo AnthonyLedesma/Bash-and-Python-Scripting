ab3f6719fe82704fbac1d802b2995f88ff187fd6
paranoia1906 <anthonymledesma@gmail.com>
2019-02-05 09:04:53 -0700

    New method for handling errors added to exceptional.py

--- python/exceptional.py
+++ python/exceptional.py
@@ -1,12 +1,22 @@
 #!/usr/bin/enb python3
 '''A module for demonstrating exceptions.'''
+import sys
+#def convert(s):
+#	'''Convert to an integer.'''
+#	try:
+#		x = int(s)
+#		print("Conversion succeeded!")
+#	except (ValueError, TypeError):
+#		x = -1
+#		print("Conversion failed")
+#	return x
 
 def convert(s):
 	'''Convert to an integer.'''
 	try:
-		x = int(s)
-		print("Conversion succeeded!")
-	except (ValueError, TypeError):
-		x = -1
-		print("Conversion failed")
-	return x
+		return int(s)
+	except (ValueError, TypeError) as err:
+		print("Conversion error: {}"\
+			.format(str(err)),
+			file=sys.stderr)
+	return -1
