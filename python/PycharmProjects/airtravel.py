64af9ebbb31e7bd780f02562c358ebc4ddf65396
paranoia1906 <anthonymledesma@gmail.com>
2019-02-07 09:15:50 -0700

    Made new parent class Aircraft and turned the specific aircraft classes into subclasses of Aircraft. This way we can share base methods across all subclasses.

--- python/PycharmProjects/airtravel.py
+++ python/PycharmProjects/airtravel.py
@@ -107,15 +107,23 @@ class Flight:
             for letter in seat_letters:
                 passenger = self._seating[row][letter]
                 if passenger is not None:
-                    yield (passenger, "{}{}".format(row,letter))
+                    yield (passenger, "{}{}".format(row, letter))
 
-class AirbusA319:
+
+class Aircraft:
 
     def __init__(self, registration):
         self._registration = registration
 
     def registration(self):
         return self._registration
+
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len(row_seats)
+
+
+class AirbusA319(Aircraft):
 
     def model(self):
         return "Airbus A319"
@@ -123,13 +131,8 @@ class AirbusA319:
     def seating_plan(self):
         return range(1, 23), "ABCDEF"
 
-class Boeing777:
 
-    def __init__(self, registration):
-        self._registration = registration
-
-    def registration(self):
-        return self._registration
+class Boeing777(Aircraft):
 
     def model(self):
         return "Boeing 777"
