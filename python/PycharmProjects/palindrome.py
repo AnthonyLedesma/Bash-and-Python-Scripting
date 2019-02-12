9a3fdbfae3db655ad6b72d1e3e2133c7eb006a11
paranoia1906 <anthonymledesma@gmail.com>
2019-02-12 07:15:31 -0700

    Used pdb.set_trace() to debug palindrome.py script.

--- python/PycharmProjects/palindrome.py
+++ python/PycharmProjects/palindrome.py
@@ -13,11 +13,13 @@ def digits(x):
     [3, 2, 1, 1, 2, 3]
     """
 
+    # import pdb; pdb.set_trace()
+
     digs = []
     while x != 0:
         div, mod = divmod(x, 10)
         digs.append(mod)
-        x = mod
+        x = div
     return digs
 
 
@@ -51,6 +53,12 @@ class Tests(unittest.TestCase):
     def test_positive(self):
         """Check that it works for single digit numbers."""
 
+    def test_single_digit(self):
+        """Chcek that it works for single digit numbers."""
+        for i in range(10):
+            self.assertTrue(is_palindrome(i))
 
 
+if __name__ == '__main__':
+    unittest.main()
 
